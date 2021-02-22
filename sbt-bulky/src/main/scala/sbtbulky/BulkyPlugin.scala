ad8009a00fe55b8594c4795e836538337e9dd3d7
Alex Glowacki <klopsmonster@gmail.com>
2021-02-22 15:06:33 +0100

    BulkyPlugin - done

--- sbt-bulky/src/main/scala/sbtbulky/BulkyPlugin.scala
+++ sbt-bulky/src/main/scala/sbtbulky/BulkyPlugin.scala
@@ -3,13 +3,13 @@ package sbtbulky
 import sbt.Keys._
 import sbt._
 import sbt.complete.DefaultParsers._
-import sbt.internal.util.complete.Parsers.spaceDelimited
+import sbt.internal.util.complete.Parser
 
 import java.io.File
 import java.nio.file.Files
 
 object BulkyPlugin extends AutoPlugin {
-  //lazy val optionalUnsignedParser: Parser[Option[Int]] = NatBasic.?
+  lazy val optionalUnsignedParser: Parser[Option[Int]] = NatBasic.?
   //lazy val spaceParser: Parser[Seq[String]] = spaceDelimited("<threshold>")
   //lazy val optionalUnsigned: Parser[Seq[Char]] = charClass(_.isDigit)
 
@@ -24,33 +24,25 @@ object BulkyPlugin extends AutoPlugin {
     bulkyThresholdInLines := 100
   )
 
-  def convertToInt(arg: Option[String]): Option[Int] = arg match {
-    case None => Some(100)
-    case Some(arg) =>
-      if (arg.forall(_.isDigit)) {
-        val intArg = arg.toInt
-        if (intArg >= 0) Some(intArg) else None
-      } else None
-  }
-
   def sortBulkyFiles(files: Seq[File], threshold: Int): Seq[(Int, File)] =
     files
       .map { file => (Files.lines(file.toPath).count.toInt, file) }
       .filter(_._1 >= threshold)
       .sortWith(_._1 > _._1)
 
+  def thresholdParser(display: String): Parser[Option[Int]] =
+    (token(Space) ~> token(optionalUnsignedParser, display)) | token(optionalUnsignedParser, display)
+
   override lazy val projectSettings = Seq(
     Compile / bulkySources := {
-      val args = spaceDelimited("<threshold>").parsed
-      val thresholdOpt = convertToInt(args.headOption)
-      if (thresholdOpt.nonEmpty) bulkyThresholdInLines := thresholdOpt.get else failure("You have to provide an unsigned integer")
-      sortBulkyFiles((sources in Compile).value, bulkyThresholdInLines.value)
+      val thresholdOpt = thresholdParser("<threshold>").parsed
+      val threshold = thresholdOpt.getOrElse(bulkyThresholdInLines.value)
+      sortBulkyFiles((sources in Compile).value, threshold)
     },
     Test / bulkySources := {
-      val args = spaceDelimited("<threshold>").parsed
-      val thresholdOpt = convertToInt(args.headOption)
-      if (thresholdOpt.nonEmpty) bulkyThresholdInLines := thresholdOpt.get else failure("You have to provide an unsigned integer")
-      sortBulkyFiles((sources in Test).value, bulkyThresholdInLines.value)
+      val thresholdOpt = thresholdParser("<threshold>").parsed
+      val threshold = thresholdOpt.getOrElse(bulkyThresholdInLines.value)
+      sortBulkyFiles((sources in Test).value, threshold)
     }
   )
 }
